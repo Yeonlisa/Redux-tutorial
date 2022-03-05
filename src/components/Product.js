690b08c1244b1de015f3fe4c5e1aec41839e860a
yeonlisa <tkzw4869@gmail.com>
2022-03-05 18:02:38 +0900

    Incrementing and Decrementing Items From Cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { cartActions } from "../store/cart-slice";
 
 import "./Product.css";
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -37,13 +37,13 @@ const cartSlice = createSlice({
             }
         },
         removeFromCart(state, action) {
-        state.changed = true;
-        const id = action.payload;
+            state.changed = true;
+            const id = action.payload;
 
-        const existingItem = state.itemsList.find((item) => item.id === id);
-        if (existingItem.quantity === 1) {
-            state.itemsList = state.itemsList.filter((item) => item.id !== id);
-            state.totalQuantity--;
+            const existingItem = state.itemsList.find((item) => item.id === id);
+            if (existingItem.quantity === 1) {
+                state.itemsList = state.itemsList.filter((item) => item.id !== id);
+                state.totalQuantity--;
             } else {
                 existingItem.quantity--;
                 existingItem.totalPrice -= existingItem.price;
