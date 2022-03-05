68bf77f24cb991a8aec9915b8a64cf8f791b3c03
yeonlisa <tkzw4869@gmail.com>
2022-03-05 17:31:15 +0900

    Shopping Cart Project-Auth Slice State

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { actions } from './store/index';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
 
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
-  const increment = () => {
-    dispatch(actions.increment());
-  }
-  const decrement = () => {
-    dispatch(actions.decrement());
-  }
-  const addBy = () => {
-    dispatch(actions.addBy(10));
-  }
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
-    <div>
-      <h1>Counter App</h1>
-      <h2>{counter}</h2>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={addBy}>Add By 10</button>
+    <div className="App">
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,10 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./auth-slice";
 
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState : { counter : 0 },
-    reducers: {
-        increment(state, action) {
-            state.counter++;
-        },
-        decrement(state, action) {
-            state.counter--;
-        },
-        addBy(state, action) {
-            state.counter += action.payload;
-        }
-    }
-})
-export const actions = counterSlice.actions;
+
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {
+        auth: authSlice.reducer,
+    },
 });
 export default store;
