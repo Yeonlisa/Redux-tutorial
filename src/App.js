0b664f1bb4a3dc61205ad392d67df47de79792f8
yeonlisa <tkzw4869@gmail.com>
2022-03-05 18:18:53 +0900

    Sending asynchronous HTTP Requests with Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { fetchData, sendCartData } from "./store/cart-actions";
 
 function App() {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
