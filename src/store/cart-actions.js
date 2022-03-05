1e91a15f75d2847e41b94e10a514e3fa0aa144cb
yeonlisa <tkzw4869@gmail.com>
2022-03-05 18:41:34 +0900

    Creating Thunk Pattern with redux

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -4,11 +4,11 @@ import { uiActions } from "./ui-slice";
 export const fetchData = () => {
     return async (dispatch) => {
         const fetchHandler = async () => {
-        const res = await fetch(
-            "https://redux-http-9ed75-default-rtdb.firebaseio.com/cartItems.json"
-        );
-        const data = await res.json();
-        return data;
+            const res = await fetch(
+                "https://redux-http-9ed75-default-rtdb.firebaseio.com/cartItems.json"
+            );
+            const data = await res.json();
+            return data;
         };
         try {
             const cartData = await fetchHandler();
@@ -58,7 +58,7 @@ export const sendCartData = (cart) => {
         try {
             await sendRequest();
         } catch (err) {
-        dispatch(
+            dispatch(
                 uiActions.showNotification({
                     open: true,
                     message: "Sending Request Failed",
